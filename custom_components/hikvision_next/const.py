1699324b57e5f09da421ae059eb757f0a3b0d1c3
Mark Parker <msparker@sky.com>
2023-05-26 21:32:17 +0100

    add mutex check

--- custom_components/hikvision_next/const.py
+++ custom_components/hikvision_next/const.py
@@ -19,7 +19,6 @@ EVENT_SWITCH_LABEL_FORMAT = "{} Detection"
 HOLIDAY_MODE_SWITCH_LABEL = "Holiday mode"
 ALARM_SERVER_SENSOR_LABEL_FORMAT = "Alarm Server {}"
 
-DEVICE_TYPE_NVR = ["NVR", "DVR"]
 DEVICE_TYPE_IP_CAMERA = "IPCamera"
 DEVICE_TYPE_ANALOG_CAMERA = "AnalogCamera"
 
@@ -30,6 +29,7 @@ EVENTS = {
         "type": EVENT_BASIC,
         "label": "Motion",
         "slug": "motionDetection",
+        "mutex": True,
         "device_class": BinarySensorDeviceClass.MOTION,
     },
     "tamperdetection": {
@@ -48,18 +48,21 @@ EVENTS = {
         "type": EVENT_SMART,
         "label": "Scene Change",
         "slug": "SceneChangeDetection",
+        "mutex": True,
         "device_class": BinarySensorDeviceClass.TAMPER,
     },
     "fielddetection": {
         "type": EVENT_SMART,
         "label": "Intrusion",
         "slug": "FieldDetection",
+        "mutex": True,
         "device_class": BinarySensorDeviceClass.MOTION,
     },
     "linedetection": {
         "type": EVENT_SMART,
         "label": "Line Crossing",
         "slug": "LineDetection",
+        "mutex": True,
         "device_class": BinarySensorDeviceClass.MOTION,
     },
     "regionentrance": {
