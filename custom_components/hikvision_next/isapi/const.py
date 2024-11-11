47a7417fb5eb7f22ca8b880ca34afd38e3f713d9
sebastian-enz <5671344+sebastian-enz@users.noreply.github.com>
2024-11-11 11:40:22 +0100

    Add separte thermometry event

--- custom_components/hikvision_next/isapi/const.py
+++ custom_components/hikvision_next/isapi/const.py
@@ -69,6 +69,11 @@ EVENTS = {
         "slug": "WLAlarm/PIR",
         "direct_node": "PIRAlarm",
     },
+    "thermometry": {
+        "type": EVENT_BASIC,
+        "label": "Thermometry",
+        "slug": "thermometry",
+    },
 }
 
 STREAM_TYPE = {
@@ -81,7 +86,6 @@ STREAM_TYPE = {
 
 EVENTS_ALTERNATE_ID = {
     "vmd": "motiondetection",
-    "thermometry": "motiondetection",
     "shelteralarm": "tamperdetection",
     "VMDHumanVehicle": "motiondetection",
 }
